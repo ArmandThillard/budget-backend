a9a7e20df5029f209399567851b50c080bf066b2
Armand Thillard <armand.thillard@proton.me>
2022-09-08 16:33:33 +0200

    fix(transaction): json normalization for response

--- src/Controller/TransactionController.php
+++ src/Controller/TransactionController.php
@@ -3,24 +3,29 @@
 namespace App\Controller;
 
 use App\Repository\TransactionRepository;
-use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class TransactionController extends AbstractController
 {
 
     #[Route('/transaction', name: 'transaction_list', methods: 'GET')]
-    public function getTransactions(TransactionRepository $transactionRepository): Response
+    public function getTransactions(TransactionRepository $transactionRepository, NormalizerInterface $normalizer): Response
     {
-        $transactionsArray = $transactionRepository->findAll();
+        $transactions = $transactionRepository->findAll();
 
-        $status = empty($transactionsArray) ? 404 : 204;
+        $suppliersNormalized = $normalizer->normalize($transactions, 'json', ["groups" => "show_transaction"]);
 
-        $response = new JsonResponse($transactionsArray);
-        $response->headers->set('Content-Type', 'application/json', $status);
+        $json = json_encode($suppliersNormalized);
+
+        $status = empty($transactions) ? 204 : 200;
+
+        $response = new Response($json, $status, [
+            "Content-Type" => "application/json"
+        ]);
 
         return $response;
     }
