4885dca503b62e37bb43585db089d66f0757f79d
Armand Thillard <armand.thillard@proton.me>
2022-10-10 23:04:27 +0200

    feat(files): save file from request

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\File;
 use App\Repository\FileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
@@ -27,5 +29,24 @@ class FileController extends AbstractController
         ]);
 
         return $response;
+    }
+
+    #[Route('/file', name: 'upload_file', methods: 'POST')]
+    public function uploadFile(Request $request, FileRepository $fileRepository): Response
+    {
+        $content = json_decode($request->getContent());
+        $filename = $content->filename;
+        $data = $content->data;
+
+        $projectDir = $this->getParameter('kernel.project_dir');
+
+        file_put_contents($projectDir . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . $filename, $data);
+        // $status = is_file($file) ? 201 : 400;
+
+        $fileEntity = new File();
+
+        // $fileEntity->setName($file->originalName);
+        // $fileEntity->setPath();
+        return new Response('', 201);
     }
 }
