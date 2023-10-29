79e4ff6f1f400d888a608224eb7a2be974347f49
ArmandThillard <thillard.armand@gmail.com>
2023-10-29 16:40:19 +0100

    fix(fixtures): add values for file table columns

--- src/DataFixtures/categoryFixtures.sql
+++ src/DataFixtures/categoryFixtures.sql
@@ -1,3 +1,5 @@
 INSERT INTO category (category_id,label, parent_category_id)
-VALUES (0, 'category label test', null)
+VALUES
+(0, 'parent category label test', null),
+(1, 'category label test', 0)
 ;
