2abacb3a5321dadf8e8d9889c66fc27493587fae
ArmandThillard <thillard.armand@gmail.com>
2023-11-19 11:12:37 +0100

    feat(file): add FileCOntroller testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,12 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
+if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
+    require dirname(__DIR__) . '/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
 
 if ($_SERVER['APP_DEBUG']) {
@@ -16,26 +16,12 @@ if ($_SERVER['APP_DEBUG']) {
 
 // executes the "php bin/console cache:clear" command
 passthru(sprintf(
-  'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
-  $_ENV['APP_ENV'],
-  __DIR__
+    'APP_ENV=%s php "%s/../bin/console" cache:clear --no-warmup',
+    $_ENV['APP_ENV'],
+    __DIR__
 ));
 
-// // executes the "php bin/console --env=test doctrine:database:create" command
-// passthru(sprintf(
-//     'APP_ENV=%s php "%s/../bin/console" doctrine:database:create',
-//     $_ENV['APP_ENV'],
-//     __DIR__
-// ));
-
-// // executes the "php bin/console --env=test doctrine:schema:create" command
-// passthru(sprintf(
-//     'APP_ENV=%s php "%s/../bin/console" doctrine:schema:create',
-//     $_ENV['APP_ENV'],
-//     __DIR__
-// ));
-
-// executes the "php bin/console --env=test doctrine:schema:create" command
+// executes the "php bin/console --env=test doctrine:fixtures:load" command
 passthru(sprintf(
     'APP_ENV=%s php "%s/../bin/console" --env=test --no-interaction doctrine:fixtures:load',
     $_ENV['APP_ENV'],
